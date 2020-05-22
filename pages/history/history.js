0f40ab0210d06d329a94cf16bb2da9edd00d39dc
waterbrother2019122132 <56429043+waterbrother2019122132@users.noreply.github.com>
2020-05-22 23:07:53 +0800

    布局大致弄好

---
+++ pages/history/history.js
@@ -0,0 +1,108 @@
+Component({
+  options: {
+    addGlobalClass: true,
+    TabCur: 0,
+    scrollLeft: 0,
+    currentIndex: 0,
+    "firstList": ["LXT", "LXT", "LXT", "LXT", "LXT", "LXT"],
+    "secondList": ["GFF", "GFF", "GFF", "GFF", "GFF", "GFF", "GFF", "GFF"]
+  },
+  data: {
+    starCount: 0,
+    forksCount: 0,
+    visitTotal: 0,
+  },
+  attached() {
+    console.log("success")
+    let that = this;
+    wx.showLoading({
+      title: '数据加载中',
+      mask: true,
+    })
+    let i = 0;
+    numDH();
+    function numDH() {
+      if (i < 20) {
+        setTimeout(function () {
+          that.setData({
+            starCount: i,
+            forksCount: i,
+            visitTotal: i
+          })
+          i++
+          numDH();
+        }, 20)
+      } else {
+        that.setData({
+          starCount: that.coutNum(3000),
+          forksCount: that.coutNum(484),
+          visitTotal: that.coutNum(24000)
+        })
+      }
+    }
+    wx.hideLoading()
+  },
+  methods: {
+    coutNum(e) {
+      if (e > 1000 && e < 10000) {
+        e = (e / 1000).toFixed(1) + 'k'
+      }
+      if (e > 10000) {
+        e = (e / 10000).toFixed(1) + 'W'
+      }
+      return e
+    },
+    CopyLink(e) {
+      wx.setClipboardData({
+        data: e.currentTarget.dataset.link,
+        success: res => {
+          wx.showToast({
+            title: '已复制',
+            duration: 1000,
+          })
+        }
+      })
+    },
+    showModal(e) {
+      this.setData({
+        modalName: e.currentTarget.dataset.target
+      })
+    },
+    hideModal(e) {
+      this.setData({
+        modalName: null
+      })
+    },
+    showQrcode() {
+      wx.previewImage({
+        urls: ['https://image.weilanwl.com/color2.0/zanCode.jpg'],
+        current: 'https://image.weilanwl.com/color2.0/zanCode.jpg' // 当前显示图片的http链接      
+      })
+    },
+    
+    tabSelect(e) {
+      this.setData({
+        TabCur: e.currentTarget.dataset.id,
+        scrollLeft: (e.currentTarget.dataset.id - 1) * 60
+      })
+    },
+    pagechange: function (e) {
+      if ("touch" === e.detail.source) {
+        let currentPageIndex = this.data.currentIndex
+        currentPageIndex = (currentPageIndex + 1) % 2
+        this.setData({
+          currentIndex: currentPageIndex
+        })
+      }
+    },
+    //用户点击tab时调用
+    titleClick: function (e) {
+      let currentPageIndex =
+        this.setData({
+          //拿到当前索引并动态改变
+          currentIndex: e.currentTarget.dataset.idx
+        })
+    }
+    
+  }
+})
