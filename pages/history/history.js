bed39add8ba82a68aa0556fc69c4f25bd0531fcc
waterbrother2019122132 <56429043+waterbrother2019122132@users.noreply.github.com>
2020-05-23 13:30:51 +0800

    选项卡我做成了组件

--- pages/history/history.js
+++ pages/history/history.js
@@ -1,48 +1,70 @@
 Component({
   options: {
     addGlobalClass: true,
-    TabCur: 0,
-    scrollLeft: 0,
-    currentIndex: 0,
-    "firstList": ["LXT", "LXT", "LXT", "LXT", "LXT", "LXT"],
-    "secondList": ["GFF", "GFF", "GFF", "GFF", "GFF", "GFF", "GFF", "GFF"]
   },
   data: {
     starCount: 0,
     forksCount: 0,
     visitTotal: 0,
+    tabs: [
+      { id: 0, title: '全部', isActive: true },
+      { id: 1, title: '待付款', isActive: false },
+      { id: 2, title: '代发货', isActive: false },
+      { id: 2, title: '退款/退货', isActive: false }
+    ]
+   
+    // currentIndex: 0,
+    // "firstList": ["LXT", "LXT", "LXT", "LXT", "LXT", "LXT"],
+    // "secondList": ["GFF", "GFF", "GFF", "GFF", "GFF", "GFF", "GFF", "GFF"]
   },
+  
   attached() {
-    console.log("success")
-    let that = this;
+    console.log('success')
+    let that = this
     wx.showLoading({
       title: '数据加载中',
       mask: true,
     })
-    let i = 0;
-    numDH();
+    let i = 0
+    numDH()
     function numDH() {
       if (i < 20) {
         setTimeout(function () {
           that.setData({
             starCount: i,
             forksCount: i,
-            visitTotal: i
+            visitTotal: i,
           })
           i++
-          numDH();
+          numDH()
         }, 20)
       } else {
         that.setData({
           starCount: that.coutNum(3000),
           forksCount: that.coutNum(484),
-          visitTotal: that.coutNum(24000)
+          visitTotal: that.coutNum(24000),
         })
       }
     }
     wx.hideLoading()
   },
   methods: {
+    handleTabsChange(e) {
+      // console.log(e)
+      // 获取当前点击元素的索引
+      const { index } = e.detail;
+      // 获取tabs
+      const { tabs } = this.data
+      tabs.forEach((e, i) => i === index ? e.isActive = true : e.isActive = false);
+      // 把tabs的值重新赋值
+      this.setData({
+        tabs
+      })
+    },
+    tabClick(val) {
+      this.activeIndex = val
+      this.$apply()
+    },
     coutNum(e) {
       if (e > 1000 && e < 10000) {
         e = (e / 1000).toFixed(1) + 'k'
@@ -55,54 +77,48 @@ Component({
     CopyLink(e) {
       wx.setClipboardData({
         data: e.currentTarget.dataset.link,
-        success: res => {
+        success: (res) => {
           wx.showToast({
             title: '已复制',
             duration: 1000,
           })
-        }
+        },
       })
     },
     showModal(e) {
       this.setData({
-        modalName: e.currentTarget.dataset.target
+        modalName: e.currentTarget.dataset.target,
       })
     },
     hideModal(e) {
       this.setData({
-        modalName: null
+        modalName: null,
       })
     },
     showQrcode() {
       wx.previewImage({
         urls: ['https://image.weilanwl.com/color2.0/zanCode.jpg'],
-        current: 'https://image.weilanwl.com/color2.0/zanCode.jpg' // 当前显示图片的http链接      
+        current: 'https://image.weilanwl.com/color2.0/zanCode.jpg', // 当前显示图片的http链接
       })
     },
-    
-    tabSelect(e) {
-      this.setData({
-        TabCur: e.currentTarget.dataset.id,
-        scrollLeft: (e.currentTarget.dataset.id - 1) * 60
-      })
-    },
+
     pagechange: function (e) {
-      if ("touch" === e.detail.source) {
+      if ('touch' === e.detail.source) {
         let currentPageIndex = this.data.currentIndex
         currentPageIndex = (currentPageIndex + 1) % 2
         this.setData({
-          currentIndex: currentPageIndex
+          currentIndex: currentPageIndex,
         })
       }
     },
     //用户点击tab时调用
     titleClick: function (e) {
-      let currentPageIndex =
-        this.setData({
-          //拿到当前索引并动态改变
-          currentIndex: e.currentTarget.dataset.idx
-        })
-    }
-    
-  }
+      let currentPageIndex = this.setData({
+        //拿到当前索引并动态改变
+        currentIndex: e.currentTarget.dataset.idx,
+      })
+    },
+  },
+  // 点击tab栏切换样式
+
 })
