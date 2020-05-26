ae3cda91e3a63fedf5ec5f7010fd8d4124e037a8
waterbrother2019122132 <56429043+waterbrother2019122132@users.noreply.github.com>
2020-05-26 23:46:18 +0800

    history 文件
    最近浏览页面修改

--- pages/history/history.js
+++ pages/history/history.js
@@ -6,106 +6,26 @@ Component({
     starCount: 0,
     forksCount: 0,
     visitTotal: 0,
-    tabs: [
-      { id: 0, title: '全部', isActive: true },
-      { id: 1, title: '待付款', isActive: false },
-      { id: 2, title: '代发货', isActive: false },
-      { id: 2, title: '退款/退货', isActive: false }
-    ]
-   
-    // currentIndex: 0,
-    // "firstList": ["LXT", "LXT", "LXT", "LXT", "LXT", "LXT"],
-    // "secondList": ["GFF", "GFF", "GFF", "GFF", "GFF", "GFF", "GFF", "GFF"]
-  },
-  
-  attached() {
-    console.log('success')
-    let that = this
-    wx.showLoading({
-      title: '数据加载中',
-      mask: true,
-    })
-    let i = 0
-    numDH()
-    function numDH() {
-      if (i < 20) {
-        setTimeout(function () {
-          that.setData({
-            starCount: i,
-            forksCount: i,
-            visitTotal: i,
-          })
-          i++
-          numDH()
-        }, 20)
-      } else {
-        that.setData({
-          starCount: that.coutNum(3000),
-          forksCount: that.coutNum(484),
-          visitTotal: that.coutNum(24000),
-        })
-      }
-    }
-    wx.hideLoading()
+    currentIndex: 0,
+    // 那个选项卡的暂定数据
+    firstList: [
+      { name: 'w券1', money: '5.00' },
+      { name: 'w券2', money: '50.00' },
+    ],
+    secondList: [
+      { name: 'y券1', money: '10.00' },
+      { name: 'y券2', money: '20.00' },
+    ],
+    thirdList: [
+      { name: 'g券1', money: '30.00' },
+      { name: 'g券2', money: '40.00' },
+    ],
   },
   methods: {
-    handleTabsChange(e) {
-      // console.log(e)
-      // 获取当前点击元素的索引
-      const { index } = e.detail;
-      // 获取tabs
-      const { tabs } = this.data
-      tabs.forEach((e, i) => i === index ? e.isActive = true : e.isActive = false);
-      // 把tabs的值重新赋值
-      this.setData({
-        tabs
-      })
-    },
-    tabClick(val) {
-      this.activeIndex = val
-      this.$apply()
-    },
-    coutNum(e) {
-      if (e > 1000 && e < 10000) {
-        e = (e / 1000).toFixed(1) + 'k'
-      }
-      if (e > 10000) {
-        e = (e / 10000).toFixed(1) + 'W'
-      }
-      return e
-    },
-    CopyLink(e) {
-      wx.setClipboardData({
-        data: e.currentTarget.dataset.link,
-        success: (res) => {
-          wx.showToast({
-            title: '已复制',
-            duration: 1000,
-          })
-        },
-      })
-    },
-    showModal(e) {
-      this.setData({
-        modalName: e.currentTarget.dataset.target,
-      })
-    },
-    hideModal(e) {
-      this.setData({
-        modalName: null,
-      })
-    },
-    showQrcode() {
-      wx.previewImage({
-        urls: ['https://image.weilanwl.com/color2.0/zanCode.jpg'],
-        current: 'https://image.weilanwl.com/color2.0/zanCode.jpg', // 当前显示图片的http链接
-      })
-    },
-
     pagechange: function (e) {
       if ('touch' === e.detail.source) {
         let currentPageIndex = this.data.currentIndex
-        currentPageIndex = (currentPageIndex + 1) % 2
+        currentPageIndex = (currentPageIndex + 1) % 3
         this.setData({
           currentIndex: currentPageIndex,
         })
@@ -119,6 +39,4 @@ Component({
       })
     },
   },
-  // 点击tab栏切换样式
-
 })
